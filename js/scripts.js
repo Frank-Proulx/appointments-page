de945c8cb4d33aed1492f0a25bbc4fb959d313a1
Frank-Proulx <frankproulx7@gmail.com>
2021-10-20 21:27:58 -0700

    add some styling, a photo and variables to capture input

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,14 @@
 
 
 $(document).ready(function() {
+  $("#appointments").submit(function() {
+    const name = $("input#name").val();
+    const description = $("input#description").val();
+    const date = $("input#date").val();
+    const startTime = $("input#start-time").val();
+    const endTime = $("input#end-time").val();
 
+  }); 
 
 
 
