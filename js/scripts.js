32ac274aed659810c4ec44ce0b12118a473c9d13
Frank-Proulx <frankproulx7@gmail.com>
2021-10-21 16:16:18 -0700

    add outputs for entered info, jquery to execute this, and readme info

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 
 
-
-
-
 $(document).ready(function() {
-  $("#appointments").submit(function() {
+  $("#appointments").submit(function(event) {
     const name = $("input#name").val();
     const description = $("input#description").val();
     const date = $("input#date").val();
     const startTime = $("input#start-time").val();
     const endTime = $("input#end-time").val();
-
+    $("#output-name").text(name);
+    $("#output-description").text(description);
+    $("#output-date").text(date);
+    $("#output-start-time").text(startTime);
+    $("#output-end-time").text(endTime);
+    event.preventDefault();
   }); 
 
 
